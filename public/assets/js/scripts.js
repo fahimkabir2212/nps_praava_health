f4c70a65d47265e3bb233b66c53c69b272cd13c5
Fahim Kabir <fahimkabir2213@gmail.com>
2023-12-27 17:54:40 +0600

    Fixed the second screen

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,1 +1,15 @@
-console.log('TEST')
+document.addEventListener('DOMContentLoaded', function() {
+    // Listen for checkbox changes
+    var item1Checkbox = document.getElementById('item_1');
+    item1Checkbox.addEventListener('change', function() {
+        updateTestBlockVisibility(item1Checkbox);
+    });
+
+    // Function to update the visibility of the test-block
+    function updateTestBlockVisibility(checkbox) {
+        var testBlock = document.querySelector('.test-block');
+
+        // Show or hide the test-block based on whether item 1 is checked
+        testBlock.style.display = checkbox.checked ? 'block' : 'none';
+    }
+});
--- resources/views/pages/NpsFormScreen2.blade.php
+++ resources/views/pages/NpsFormScreen2.blade.php
@@ -5,14 +5,14 @@
             <div class="vh-100 form-steps">
                 <div class="steps">
                     <div class="step">
-                        <a href="{{route('main.form.slide1')}}">
+                        <a href="{{ route('main.form.slide1') }}">
                             <h3>1</h3>
                         </a>
                         <p>STEP</p>
                     </div>
                     <div class="step">
-                        <a href="{{route('main.form.slide2')}}">
-                            <h3  class="active-step">2</h3>
+                        <a href="{{ route('main.form.slide2') }}">
+                            <h3 class="active-step">2</h3>
                         </a>
                         <p>STEP</p>
                     </div>
@@ -31,52 +31,7 @@
                 </div>
             </div>
             <div class="form-container">
-                {{-- <div class="review-form">
-                    <p class="question mb-2x">From 1 to 10, how likely are you to recommend Praava Health to friends or
-                        family? </p>
-                    <div class="rating-container">
-                        <div class="rating-item">
-                            <input type="radio" id="rating1" name="rating" value="1">
-                            <label for="rating1">1</label>
-                        </div>
-                        <div class="rating-item">
-                            <input type="radio" id="rating2" name="rating" value="2">
-                            <label for="rating2">2</label>
-                        </div>
-                        <div class="rating-item">
-                            <input type="radio" id="rating3" name="rating" value="3">
-                            <label for="rating3">3</label>
-                        </div>
-                        <div class="rating-item">
-                            <input type="radio" id="rating4" name="rating" value="4">
-                            <label for="rating4">4</label>
-                        </div>
-                        <div class="rating-item">
-                            <input type="radio" id="rating5" name="rating" value="5">
-                            <label for="rating5">5</label>
-                        </div>
-                        <div class="rating-item">
-                            <input type="radio" id="rating6" name="rating" value="6">
-                            <label for="rating6">6</label>
-                        </div>
-                        <div class="rating-item">
-                            <input type="radio" id="rating7" name="rating" value="7">
-                            <label for="rating7">7</label>
-                        </div>
-                        <div class="rating-item">
-                            <input type="radio" id="rating8" name="rating" value="8">
-                            <label for="rating8">8</label>
-                        </div>
-                        <div class="rating-item">
-                            <input type="radio" id="rating9" name="rating" value="9">
-                            <label for="rating9">9</label>
-                        </div>
-                        <div class="rating-item">
-                            <input type="radio" id="rating10" name="rating" value="10">
-                            <label for="rating10">10</label>
-                        </div>
-                    </div>
-                </div> --}}
+
                 <div class="review-form">
                     <p class="question mb-2x">Who did you interact with at Praava today? Select all that apply.</p>
                     <div class="main-response-reason">
@@ -84,17 +39,17 @@
                             <input type="checkbox" id="item_1">
                             <label class="btn btn-warning button_select" for="item_1">Contact Center</label>
                         </div>
-                        
+
                         <div class="select">
                             <input type="checkbox" id="item_2">
                             <label class="btn btn-warning button_select" for="item_2">Praava's social media pages</label>
                         </div>
-                        
+
                         <div class="select">
                             <input type="checkbox" id="item_3">
                             <label class="btn btn-warning button_select" for="item_3">Nurse</label>
                         </div>
-                        
+
                         <div class="select">
                             <input type="checkbox" id="item_4">
                             <label class="btn btn-warning button_select" for="item_4">Reception Team</label>
@@ -105,7 +60,8 @@
                         </div>
                         <div class="select">
                             <input type="checkbox" id="item_6">
-                            <label class="btn btn-warning button_select" for="item_6">X-ray, ETT, ECHO, and/or USG (Technician)</label>
+                            <label class="btn btn-warning button_select" for="item_6">X-ray, ETT, ECHO, and/or USG
+                                (Technician)</label>
                         </div>
                         <div class="select">
                             <input type="checkbox" id="item_7">
@@ -113,9 +69,12 @@
                         </div>
                     </div>
                 </div>
+                <div class="test-block" style="display: none">
+                    @include('components.reviewScale')
+                </div>
                 <div class="flex">
-                    <a href="{{route('main.form.slide1')}}" class="btn btn-orange">Prev</a>
-                <a href="{{route('main.form.slide2')}}" class="btn btn-green">Next</a>
+                    <a href="{{ route('main.form.slide1') }}" class="btn btn-orange">Prev</a>
+                    <a href="{{ route('main.form.slide2') }}" class="btn btn-green">Next</a>
                 </div>
             </div>
         </div>
