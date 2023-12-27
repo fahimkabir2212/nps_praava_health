10a9fd45ad6506ee63da34e2a134e79c95fa5356
Fahim Kabir <fahimkabir2213@gmail.com>
2023-12-27 11:25:54 +0600

    Refractored the code of landing page

--- resources/views/pages/LandingPage.blade.php
+++ resources/views/pages/LandingPage.blade.php
@@ -3,10 +3,6 @@
     <section class="section-landing-page">
         <div class="grid grid-col-4x-8x">
             <div class="vh-100 content">
-                {{-- <img src="{{ asset('assets/images/praava-logo.png') }}" alt=""> --}}
-                {{-- <h1 class="title">Feedback Form</h1>
-                <p class="description">Select your preferred language and enter your upi or phone number to give feedback.
-                </p> --}}
             </div>
             <div class="form-container">
                 <img class="prava-logo" src="{{ asset('assets/images/praava-logo-main.svg') }}" alt="">
@@ -17,7 +13,7 @@
                 </div>
                 <form class="initial-form" action="">
                     <input type="number" placeholder="search Phone number / UPI ">
-                    <button class="btn btn-primary">Start</button>
+                    <a class="btn btn-primary">Start</a>
                 </form>
 
             </div>
