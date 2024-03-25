017c485a7fd2992a3a8c2b31d93e95423e0cc4d4
Fahim Kabir <fahimkabir2213@gmail.com>
2024-03-25 13:35:03 +0600

    Changed

--- resources/views/layout/css.blade.php
+++ resources/views/layout/css.blade.php
@@ -1,6 +1,3 @@
-{{-- SELECT 2 --}}
-<link href="{{ asset('assets/css/select2/select2.min.css') }}" rel="stylesheet" />
-
 {{-- LINK CSS FILES HERE --}}
 <link href="{{ asset('assets/css/global.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/css/layout.css') }}" rel="stylesheet" />
--- resources/views/layout/js.blade.php
+++ resources/views/layout/js.blade.php
@@ -1,9 +1,5 @@
     <!-- JQUERY -->
     <script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
 
-    <!-- SELECT 2 -->
-    <script src="{{ asset('assets/js/select2/select2.full.js') }}"></script>
-    <script src="{{ asset('assets/js/selectScript.js') }}"></script>
-
     {{-- LINK JS FILES HERE --}}
     <script src="{{ asset('assets/js/scripts.js') }}"></script>
